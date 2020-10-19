b4bd6fe37f990481561bbbe5602d296926c3fc19
Andrey Medvedew <andreygasparyan539@gmail.com>
2020-10-19 18:25:00 +0300

    Update function

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -9,6 +9,7 @@ public class MainProgram
         System.out.println("Please choose operation number:");
         System.out.println("1) A method that checks the square root of a number.");
         System.out.println("2) A method that checks a number to a specified degree.");
+        System.out.println("3) Function for calculating the square root.");
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int inputTaskValue = Integer.parseInt(bufferedReader.readLine());
@@ -39,6 +40,20 @@ public class MainProgram
                 System.out.println("Something error " + ex.getMessage());
             }
         }
+        else if(inputTaskValue == 3){
+            System.out.println("Please, input three values:");
+            try
+            {
+                double firstValue = Double.parseDouble(bufferedReader.readLine());
+                double secondValue = Double.parseDouble(bufferedReader.readLine());
+                double thirdValue = Double.parseDouble(bufferedReader.readLine());
+
+                mainProgram.GetFunctionData(firstValue, secondValue, thirdValue);
+            }
+            catch(NumberFormatException ex){
+                System.out.println("Something error " + ex.getMessage());
+            }
+        }
         else
         {
             throw new Exception("Please input a valid number(1 or 2)");
@@ -64,5 +79,24 @@ public class MainProgram
     public int GetPowResult(int firstNumber, int powValue)
     {
         return (int)Math.pow(firstNumber, powValue);
+    }
+
+    public void GetFunctionData(double firstValue, double secondValue, double thirdValue){
+        double discriminant = Math.pow(secondValue, 2) + 4 * firstValue * thirdValue;
+
+        if(discriminant > 0) {
+            double firstResult = -secondValue + Math.sqrt(discriminant) / (2 * firstValue);
+            double secondResult = -secondValue - Math.sqrt(discriminant) / (2 * firstValue);
+
+            System.out.println("X1 = " + firstResult + " and X2 result is: " + secondResult);
+        }
+        else if(discriminant == 0) {
+            double firstResult = -secondValue / (2 * firstValue);
+
+            System.out.println("X1 = " + firstResult);
+        }
+        else {
+            System.out.println("No data to display");
+        }
     }
 }
